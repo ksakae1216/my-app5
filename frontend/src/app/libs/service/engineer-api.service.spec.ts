5f22499e433a84abc750bfb3864acb183de1bd8c
Sakae Kotaro <ksakae1216@gmail.com>
2020-06-10 19:48:13 +0900

    Develop merge (#24)
    * implementation detail (#11)
    
    * change mock from swagger to in-memory-mock (#13)
    
    * fix confrict
    
    * Refactor engineer-list (#19)
    
    * add login and engineer-list test (#23)

--- frontend/src/app/libs/service/engineer-api.service.spec.ts
+++ frontend/src/app/libs/service/engineer-api.service.spec.ts
@@ -1,10 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-<<<<<<< HEAD:frontend/src/app/libs/service/engineer-api.service.spec.ts
 import { EngineerApiService } from './engineer-api.service';
-=======
-import { HttpListService } from './http-engineer.service';
->>>>>>> master:frontend/src/app/libs/service/http-engineer.service.spec.ts
 
 describe('HttpListService', () => {
   let service: EngineerApiService;
