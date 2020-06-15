e2c6d5aae9f2a74ccbec3f2d10c6d8f4cc7263d3
Sakae Kotaro <ksakae1216@gmail.com>
2020-06-15 21:55:03 +0900

    Feature/add cypress imanage snapshot (#27)
    * add cypress-image-snapshot
    
    * unecessary !!!!
    
    * delete only

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,9 @@
-const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
+// const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
 
-module.exports = on => {
-  on("file:preprocessor", cypressTypeScriptPreprocessor);
+const {
+  addMatchImageSnapshotPlugin
+} = require('cypress-image-snapshot/plugin');
+
+module.exports = (on, config) => {
+  addMatchImageSnapshotPlugin(on, config);
 };
